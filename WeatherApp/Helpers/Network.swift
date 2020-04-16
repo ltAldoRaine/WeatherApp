bded5e26e1e0c34e2a8e118fc220a46ba07e1f37
Beka Gelashvili <beka.gelash@gmail.com>
2020-04-16 22:24:45 +0400

    little format changes

--- WeatherApp/Helpers/Network.swift
+++ WeatherApp/Helpers/Network.swift
@@ -12,12 +12,12 @@ class Network {
 
     static let weatherApirUrl = "https://api.openweathermap.org/data/2.5"
     static let weatherApiKey = "e7da42b9a3a035dc2a2ade8878633604"
-    
+
     static func weather(icon: String?) -> String? {
         guard let icon = icon else {
             return nil
         }
         return "https://openweathermap.org/img/wn/\(icon)@\(Int(UIScreen.main.scale))x.png"
     }
-    
+
 }
