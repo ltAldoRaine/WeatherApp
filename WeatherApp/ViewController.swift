a2c1b8d046a9dba5374bcb51964be9e9fd1f5665
Beka Gelashvili <beka.gelash@gmail.com>
2020-04-16 16:14:23 +0400

    initial commit

---
+++ WeatherApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeatherApp
+//
+//  Created by Beka Gelashvili on 4/16/20.
+//  Copyright © 2020 TBC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
