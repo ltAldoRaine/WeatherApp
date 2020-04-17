8ec76b965a28919033f9f71b324524fb9f61d6e2
Beka Gelashvili <beka.gelash@gmail.com>
2020-04-17 14:37:19 +0400

    create image class

---
+++ WeatherApp/Helpers/Image.swift
@@ -0,0 +1,17 @@
+//
+//  Image.swift
+//  WeatherApp
+//
+//  Created by Beka Gelashvili on 4/17/20.
+//  Copyright © 2020 TBC. All rights reserved.
+//
+
+import UIKit
+
+class Image {
+
+    static var noImageAvailable: UIImage? {
+        return UIImage(named: "NoImageAvailable")
+    }
+
+}
