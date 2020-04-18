ebb2e70c185052bb38b8f558fe28d24d20304c5b
Beka Gelashvili <beka.gelash@gmail.com>
2020-04-18 13:05:51 +0400

    add Inspectable properties

--- WeatherApp/Extensions/UIViewExtension.swift
+++ WeatherApp/Extensions/UIViewExtension.swift
@@ -10,6 +10,32 @@ import UIKit
 
 extension UIView {
 
+    @IBInspectable
+    var borderWidth: CGFloat {
+        get {
+            return layer.borderWidth
+        }
+        set {
+            layer.borderWidth = newValue
+        }
+    }
+    @IBInspectable
+    var borderColor: UIColor? {
+        get {
+            if let color = layer.borderColor {
+                return UIColor(cgColor: color)
+            }
+            return nil
+        }
+        set {
+            if let color = newValue {
+                layer.borderColor = color.cgColor
+            } else {
+                layer.borderColor = nil
+            }
+        }
+    }
+
     func fixInView(_ container: UIView!) {
         translatesAutoresizingMaskIntoConstraints = false
         frame = container.frame
